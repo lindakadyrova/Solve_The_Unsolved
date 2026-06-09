24403eac8877dca69d43ad11281dccd749af6068
Linda Kadyrova <linda.kadyrova@edu.fh-joanneum.at>
2026-06-09 12:36:59 +0200

    Initial Jetpack Compose project

---
+++ app/src/main/java/com/kadyrova/solve_the_unsolved/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.kadyrova.solve_the_unsolved
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kadyrova.solve_the_unsolved.ui.theme.Solve_The_UnsolvedTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Solve_The_UnsolvedTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Solve_The_UnsolvedTheme {
+        Greeting("Android")
+    }
+}
